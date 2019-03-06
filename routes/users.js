4d824b4d7ee3bdb64dd2d1c868728344a0df1f03
Ankur Rastogi <ankurras@usc.edu>
2019-03-05 18:51:38 -0800

    basic twilio calling working

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+// These are Ankur's
+const accountSid = 'ACdc94f86bec1e788b280632d85ea8ace5'; //Find these in your Twilio profile, https://www.twilio.com/console
+const authToken = 'f504ba0bf8cbee3d8891307393512f44';
+const twilioClient = require('twilio')(accountSid, authToken);
+
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/app-db', {useNewUrlParser: true});
@@ -62,6 +67,28 @@ router.post('/addUser', function(req, res, next) {
 	res.send('Thanks for posting!');
 	// var user = new User({
 	// });
+});
+
+
+router.get('/startCall', function(req, res, next) {
+
+	// I need to write the TwiML to some location that Twilio can see...
+	console.log(req.query.targetNumber);
+	twilioClient.calls
+	      .create({
+			 url: 'http://demo.twilio.com/docs/voice.xml',
+	         to: req.query.targetNumber,
+	         from: '+12244123420' //Twilio phone number
+	       },
+			  (err, call) => {
+			    process.stdout.write(call.sid);
+			  }
+	       )
+	      .then(call => console.log(call.sid))
+	      .done();
+
+  	res.status(200).send("called successfully.");
+
 });
 
 module.exports = router;
