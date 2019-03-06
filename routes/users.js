6afcc7aef00444a7ec2fc6f8dd5c613b92266a36
Ankur Rastogi <ankurras@usc.edu>
2019-03-05 19:28:15 -0800

    working on calling functionality

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const ngrok = require('ngrok');
+
+var currentCallingNumber = "+"
 
 // These are Ankur's
 const accountSid = 'ACdc94f86bec1e788b280632d85ea8ace5'; //Find these in your Twilio profile, https://www.twilio.com/console
 const authToken = 'f504ba0bf8cbee3d8891307393512f44';
 const twilioClient = require('twilio')(accountSid, authToken);
+
 
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -21,6 +25,7 @@ router.get('/addBob', function(req, res, next) {
 	var bob = new User({
 	    firstName: 'Bob',
 	    lastName: 'Smith',
+	    phoneNumber: '+12345678901',
 	    bio: 'An adventurous lad.',
 	    activePreference: 3,
 	    preferredCallDuration: 20,
@@ -70,13 +75,41 @@ router.post('/addUser', function(req, res, next) {
 });
 
 
-router.get('/startCall', function(req, res, next) {
+router.get('/twiML', function(req, res, next) {
+	const twiml = new twilioClient.twiml.VoiceResponse();
+    
+    const dial = twiml.dial({
+      callerId: '+18317041029',
+      record: 'record-from-ringing'
+    });
+
+    //srource for DIAL https://www.twilio.com/docs/voice/twiml/dial#record
+
+    dial.number(currentCallingNumber);
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+});
+
+
+router.get('/startCall', async function(req, res, next) {
+
+	try {
+		var ngrokUrl = await ngrok.connect(8000);
+		ngrokUrl = ngrokUrl + "/users/twiML";
+		console.log("ngrok url: " + ngrokUrl);
+	} catch (err) {
+		console.log(err);
+	}
 
 	// I need to write the TwiML to some location that Twilio can see...
-	console.log(req.query.targetNumber);
+	console.log("Target Number: " + req.query.targetNumber);
+	console.log("Calling Number: " + req.query.callingNumber);
+	currentCallingNumber = req.query.callingNumber;
+
 	twilioClient.calls
 	      .create({
-			 url: 'http://demo.twilio.com/docs/voice.xml',
+			 url: ngrokUrl,
 	         to: req.query.targetNumber,
 	         from: '+12244123420' //Twilio phone number
 	       },
@@ -87,6 +120,7 @@ router.get('/startCall', function(req, res, next) {
 	      .then(call => console.log(call.sid))
 	      .done();
 
+  	await ngrok.disconnect();
   	res.status(200).send("called successfully.");
 
 });
